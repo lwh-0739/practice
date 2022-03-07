bcb82931777e75d8861f1f5d0343e19c3f67ffd0
li.weihai@zhgtv.cn <982100781@qq.com>
2022-03-07 13:54:27 +0800

    添加线程池练习；

---
+++ executor-practice/src/main/java/l/w/h/executorpractice/ExecutorPractice.java
@@ -0,0 +1,21 @@
+package l.w.h.executorpractice;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author lwh
+ * @since 2022/2/22 16:16
+ **/
+public class ExecutorPractice {
+
+    // private volatile
+
+    private ThreadPoolExecutor threadPoolExecutor() throws FileNotFoundException {
+        return new ThreadPoolExecutor(5, 10, 1, TimeUnit.SECONDS, new LinkedBlockingDeque<>(20),Executors.defaultThreadFactory());
+    }
+
+}
